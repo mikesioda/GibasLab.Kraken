4c13867c83562e8d60d9bad1b3ca86853c1c2b5e
mikesioda <mikesioda@gmail.com>
2016-09-08 11:35:28 -0400

    Used to build the full Kraken DB

---
+++ kraken_build_db.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#PBS -N kraken_tbuild_db
+#PBS -l nodes=1:ppn=12
+#PBS -l vmem=95GB
+#PBS -l walltime=12:00:00
+# BUILD STANDARD KRAKEN DATABASE
+start=$(date +%s)
+echo SCRIPT BUILDING KRAKEN DB 
+KDB=/group/gibaslab/krakenDB
+kraken-build --standard --threads 12 --db $KDB
+echo KRAKEN DB BUILD IS COMPLETE
+end=$(date +%s)
+runtime=$"($end - $start)"
+echo SCRIPT COMPLETE: RUNTIME is $runtime seconds
